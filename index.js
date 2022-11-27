46200698eb890843742d8ea3127584e743d36c44
Topu bhuyan <117706660+md-topu-bhuyan@users.noreply.github.com>
2022-11-27 18:39:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// var num = "topu";
+// num = parseFloat(num);
+// console.log(typeof(num));
+/*
+var number = 2.5678;
+
+console.log(number.toString(3)) */
+//console.log(typeof(Number("12")))
+//console.log(Number("12.5"))
+console.log(Number(false))
+//console.log(Number(true))
